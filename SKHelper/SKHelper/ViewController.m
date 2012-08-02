c86aa9213671232e43b4d41ba8f40f3365d38858
Saroj Sharma <saroj.slg@gmail.com>
2012-08-02 14:44:50 +0530

    Added base code.

---
+++ SKHelper/SKHelper/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  SKHelper
+//
+//  Created by Saroj Sharma on 02/08/12.
+//  Copyright (c) 2012 _DayDreamer_. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
