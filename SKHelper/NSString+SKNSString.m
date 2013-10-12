eaa65e2bf7ae3c68067ea2a01ec0dc2b415ca8ab
Saroj <sarojsharma@Sarojs-MacBook-Pro.local>
2013-10-12 14:44:42 +0530

    Added category on NSString

---
+++ SKHelper/NSString+SKNSString.m
@@ -0,0 +1,13 @@
+//
+//  NSString+SKNSString.m
+//  SKHelper
+//
+//  Created by Saroj Sharma on 12/10/13.
+//  Copyright (c) 2013 _DayDreamer_. All rights reserved.
+//
+
+#import "NSString+SKNSString.h"
+
+@implementation NSString (SKNSString)
+
+@end
